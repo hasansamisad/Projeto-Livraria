c5ec9764ffc312b339ce5f1207de5fca240fb8b8
hasansamisad <hasan.trabalho@gmail.com>
2026-06-13 22:17:17 -0300

    fix: tornando a url do appConfig dinamica para producao

--- livraria-api/src/config/appConfig.js
+++ livraria-api/src/config/appConfig.js
@@ -1,3 +1,7 @@
 export default {
-  url: 'http://localhost:3001',
+  // Se a aplicação estiver na nuvem (Render), usa a URL do
+  // backend. Caso contrário, usa o localhost.
+  url: process.env.NODE_ENV === 'production'
+    ? 'https://projeto-livraria-backend.onrender.com'
+    : `http://localhost:${process.env.PORT || 3001}`,
 };
